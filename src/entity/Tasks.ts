3513eeb55cad4887055cabb7ee0a3fe83f0c16f9
Monique <moniquesantosmota900@gmail.com>
2021-10-19 22:10:42 -0300

    Deploy no Heroku

---
+++ src/entity/Tasks.ts
@@ -0,0 +1,27 @@
+import { create } from 'domain';
+import { Column , CreateDateColumn,Entity, PrimaryGeneratedColumn , UpdateDateColumn } from 'typeorm';
+
+@Entity()
+
+export class Tasks{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column({
+        default: false
+    })
+    finished:boolean;
+
+    @CreateDateColumn()
+    create_at:Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+    
+}
